21f2aaa5afa698277da622c59d2afca86934a9a4
Devitgg <txux.7jj@gmail.com>
2017-10-21 15:28:34 -0400

    Added Features
    did some stuff

--- Larabot/lib/responses.py
+++ Larabot/lib/responses.py
@@ -18,9 +18,21 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 ####################################################################################
-
+from Larabot.lib import config as configureThe
 import discord
 
+########### Configuration ###########
+anonCommand             = configureThe.anonCommand()
+showRoleCommand         = configureThe.showRoleCommand()
+searchCommand           = configureThe.searchCommand()
+codeCommand             = configureThe.codeCommand()
+googleResultCount       = configureThe.googleResultCount()
+plusRepCommand          = configureThe.plusRepCommand()
+viewRepCommand          = configureThe.viewRepCommand()
+
+########### Configuration ###########
+
+########### Responses ###########
 def kicked(message, user):
     embed = discord.Embed(title=message.server.name + " :: Notification", url='https://github.com/Devitgg/larabot',
                           description="You have been kicked from message.server.name")
@@ -37,7 +49,65 @@ def helpThing(message):
     embed.set_author(name="Larabot", url='https://github.com/Devitgg/larabot',
                      icon_url='https://png.icons8.com/wired/1600/D32F2F/source-code')
     embed.set_thumbnail(url='https://www.shareicon.net/download/2015/11/13/671320_people_512x512.png')
-    embed.add_field(name="Command `.roles`", value='Available in #request_roles', inline=False)
-    embed.add_field(name="Command `g>`", value='Just google it and displays top 5 Results \n\n `g> how do i google this?`', inline=False)
-    embed.set_footer(text="Open Source MIT License by Devitgg")
+    embed.add_field(name='Command: ' + codeCommand, value='Easily show syntax highlighting for specific languages \n'
+                                                                                   'Example: ```> php \n<code here>```', inline=False)
+    embed.add_field(name='Command: ' + searchCommand, value='Just google it and displays top ' + str(googleResultCount)
+                                                            + ' Results. \n `' + searchCommand + ' how do i google this?`', inline=False)
+    embed.add_field(name='Command: ' + anonCommand, value='Private message Larabot directly with \n `' + anonCommand + ' <a confession>`', inline=False)
+    embed.add_field(name='Command: ' + plusRepCommand, value='Helped by someone? Plus rep them! \n'
+                                                             '`' + plusRepCommand + ' @DΞVITGG#4809`', inline=False)
+    embed.add_field(name='Command: ' + viewRepCommand, value='Check out a users rep! \n'
+                                                             '`' + viewRepCommand + ' @DΞVITGG#4809`', inline=False)
+    embed.add_field(name='Abusing Rep', value='Abusing Rep or purposely trolling someone by @mention them will result in a kick', inline=False)
+    embed.set_footer(text="Created with <3 by Devitgg")
     return embed
+
+def anonMessage(message, anonCommand, subject):
+    theMessage = message.content.split()
+    theMessage.remove(anonCommand)
+    theMessage = ' '.join(theMessage)
+
+    embed = discord.Embed(title="Fuck my life :: Confessions of a Developer", url='https://discord.gg/Ec6ArRf',
+                          description='Subject: ' + subject)
+    embed.set_author(name="Larabot", url='https://github.com/Devitgg/larabot',
+                     icon_url='https://png.icons8.com/wired/1600/D32F2F/source-code')
+    embed.set_thumbnail(url='https://www.shareicon.net/download/2015/11/13/671320_people_512x512.png')
+    embed.add_field(name='The Confession', value='```' + theMessage + '```', inline=False)
+    embed.set_footer(text="Created with <3 by Devitgg")
+    return embed
+
+def codeType(message, codeCommand):
+    codeTypes = ['css', 'html', 'python', 'php', 'javascript', 'java']
+    for code in codeTypes:
+        if code in message.content:
+            return 'Here you go' + message.author.mention + '\n```' + code + '\n' + message.content + '```'
+
+    return 'Fix your formatting please! Its `> <coding language> <code>` .. Very simple ' + message.author.mention + '..'
+
+
+def accessServer():
+    embed = discord.Embed(title="JustDev[it] :: Welcome Message",  description='Please read & understand our rules.\n'
+                                                                               'Add a role to gain access to '
+                                                                               'our server.\n\n')
+    embed.set_author(name="Larabot", url='https://github.com/Devitgg/larabot',
+                     icon_url='https://png.icons8.com/wired/1600/D32F2F/source-code')
+    embed.set_thumbnail(url='https://www.shareicon.net/download/2015/11/13/671320_people_512x512.png')
+    embed.add_field(name='Available Roles', value='`junior dev` -  Still learning \n'
+                                                  '`senior dev` -  Guru', inline=False)
+    embed.add_field(name='How to add a role', value='Type the following: \n'
+                                                    '`add> <role>` \n \n'
+                                                    'example: \n'
+                                                    '`add> junior dev`', inline=False)
+    embed.set_footer(text="Created with <3 by Devitgg")
+    return embed
+
+
+def accessAdded(role):
+    embed = discord.Embed(title="JustDev[it] :: Access Granted",  description='You have been given access to JustDev[it].')
+    embed.set_author(name="Larabot", url='https://github.com/Devitgg/larabot',
+                     icon_url='https://png.icons8.com/wired/1600/D32F2F/source-code')
+    embed.set_thumbnail(url='https://www.shareicon.net/download/2015/11/13/671320_people_512x512.png')
+    embed.add_field(name='Role Added', value='You have been added to ' + role + '. \n Enjoy your stay!', inline=False)
+    embed.set_footer(text="Created with <3 by Devitgg")
+    return embed
+########### Responses ###########
--- Larabot/main.py
+++ Larabot/main.py
@@ -19,25 +19,34 @@
 # SOFTWARE.
 ####################################################################################
 
-from lib import config as configureThe
-from lib import responses as fetchThis
+from Larabot.lib import config as configureThe
+from Larabot.lib import responses as fetchThis
 import discord
 
 
 
 ########### Configuration ###########
-
+        ## Discord Stuff ##
 client                  = discord.Client()
 payTheBridgeToll        = configureThe.token()
-roleChannel             = configureThe.roleChannel()
+
+             ## Commands ##
+helpCommand             = configureThe.helpCommand()
+anonCommand             = configureThe.anonCommand()
 addRoleCommand          = configureThe.addRoleCommand()
 removeRoleCommand       = configureThe.removeRoleCommand()
 showRoleCommand         = configureThe.showRoleCommand()
 kickCommand             = configureThe.kickCommand()
-googleCmd               = configureThe.googleCommand()
+banCommand              = configureThe.banCommand()
+codeCommand             = configureThe.codeCommand()
+clearCommand            = configureThe.clearCommand()
+searchCommand           = configureThe.searchCommand()
+googleResultCount       = configureThe.googleResultCount()
 
+             ## Channels ##
+anonChannel             = configureThe.anonChannel()
+roleChannel             = configureThe.roleChannel()
 ########### Configuration ###########
-
 
 
 ########### Bot Information ###########
@@ -46,12 +55,10 @@ async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
+    for server in client.servers:
+        for channel in server.channels:
+            print(channel.id + '\n ' + channel.name)
     print('------')
-    for channel in client.server.channels:
-        print('All channels available \n')
-        print('Channel Name: ' + channel.name + '\n')
-        print('Channel ID: ' + channel.id+ '\n')
-        print('## \n')
 ########### Bot Information ###########
 
 
@@ -59,9 +66,11 @@ async def on_ready():
 ########### Welcome Message ###########
 @client.event
 async def on_member_join(member):
-    server = member.server
-    fmt = 'Welcome {0.mention} to {1.name}!'
-    await client.send_message(server, fmt.format(member, server))
+    for server in client.servers:
+        for channel in server.channels:
+            if channel.id == '369989443643506688':
+                response = fetchThis.accessServer()
+                await client.send_message(channel, embed=response)
 ########### Welcome Message ###########
 
 
@@ -72,27 +81,35 @@ async def on_message(message):
         return
 
 ########### HELP ###########
-    if message.content == 'help':
+    if message.content == helpCommand:
         response = fetchThis.helpThing(message)
-        await client.send_message(message.channel, embed=response)
+        this = await client.send_message(message.channel, embed=response)
+        await client.add_reaction(this, '\U0001F44D')
+        await client.add_reaction(this, '👎')
+
+    if message.content.startswith(codeCommand):
+        response = fetchThis.codeType(message, codeCommand)
+        await client.delete_message(message)
+        await client.send_message(message.channel, response)
+
 ########### User Managment ###########
 
-    # Adding a role #
-    if roleChannel in message.channel.name:
-        if message.content.startswith(addRoleCommand):
+    #### Adding a role ##
+    if message.content.startswith(addRoleCommand):
+        if roleChannel in message.channel.name:
             for role in message.server.roles:
                 if role.name in message.content:
                     await client.send_typing(message.channel)
                     await client.add_roles(message.author, role)
-                    return await client.send_message(message.channel, 'Okay, ' + message.author.name +
-                                                     'I have successfully added you to \n ```Role: '
-                                                     + role.name + '```')
+
+                    response = fetchThis.accessAdded(role.name)
+                    return await client.send_message(message.channel, embed=response)
 
             await client.send_message(message.channel, 'Sorry, not sorry but.. I couldnt perform this '
                                                        'command. \n ' + '```' + message.content + '```')
-    # Removing a role #
-    if roleChannel in message.channel.name:
-        if message.content.startswith(removeRoleCommand):
+     #### Removing a role ##
+    if message.content.startswith(removeRoleCommand):
+        if roleChannel in message.channel.name:
             for role in message.server.roles:
                 if role.name in message.content:
                     await client.send_typing(message.channel)
@@ -105,36 +122,67 @@ async def on_message(message):
                                                                       'command. \n ' + '```' + message.content + '```')
 
 
-    # Displaying the roles #
-    if roleChannel in message.channel.name:
-        if message.content.startswith(showRoleCommand):
-            await client.send_typing(message.channel)
-            return await client.send_message(message.channel, 'Okay, ' + message.author.name +
-                                                     'I have successfully removed you from \n ```Available Roles: \n'
-                                                     'Guru - Helpers \n'
-                                                     'Student - Always Learning```')
 
-    ## Kicking users ##
-    checkAuthority = configureThe.modAuthority(message)
-    if checkAuthority is True:
-        if message.content.startswith(kickCommand):
+
+############## Google Stuff ##############
+    if searchCommand in message.content:
+        from gsearch.googlesearch import search
+        query = message.content.split()
+        query.remove(searchCommand)
+        results = search(str(query))  # returns 10 or less results
+        for count in range(0, googleResultCount):
+            await client.send_message(message.channel, results[count])
+
+############## AnonMessages ##############
+
+    if message.content.startswith(anonCommand):
+        await client.send_message(message.author, 'What would you like to `Subject:` your confession?')
+
+        subject = await client.wait_for_message(timeout=60.0, author=message.author)
+
+        if subject is None:
+            await client.send_message(message.author, '`Sorry` but your confession time `ran out` (`60 Seconds`).\n'
+                                                      'Please resubmit your confession and `fill out the subject line`.')
+
+        response = fetchThis.anonMessage(message, anonCommand, subject.content)
+        for server in client.servers:
+            for channel in server.channels:
+                if anonChannel == channel.name:
+                   this = await client.send_message(channel, embed=response)
+                   await client.add_reaction(this, '\U0001F44D')
+                   await client.add_reaction(this, '👎')
+
+
+############## Server Management ##############
+
+    #### Kicking users ####
+    if message.content.startswith(kickCommand):
+        checkAuthority = configureThe.modAuthority(message)
+        if checkAuthority is True:
             for user in message.mentions:
                 response = fetchThis.kicked(message, user)
                 await client.send_message(user, embed=response)
                 await client.send_message(message.channel, user.name + ' err rip, bye felica. :unamused: ')
                 await client.kick(user)
 
+    #### Banning Users ####
+    if message.content.startswith(banCommand):
+        checkAuthority = configureThe.adminAuthority(message)
+        if checkAuthority is True:
+            for user in message.mentions:
+                response = fetchThis.banned(message, user)
+            await client.send_message(user, embed=response)
+            await client.send_message(message.channel, user.name + ' err rip, bye felica. :unamused: ')
+            await client.ban(user)
 
-########### User Managment ###########
-        ## Google Stuff ##
-    if googleCmd in message.content:
-        from gsearch.googlesearch import search
-        query = message.content.split()
-        query.remove('g>')
-        results = search(str(query))  # returns 10 or less results
-        for count in range(0, 5):
-            await client.send_message(message.channel, results[count])
-
+    #### Clear Messages ####
+    if message.content.startswith(clearCommand):
+        checkAuthority = configureThe.modAuthority(message)
+        if checkAuthority is True:
+            tmp = await client.send_message(message.channel, 'Clearing messages for you ' + message.author.name + '...')
+            async for msg in client.logs_from(message.channel):
+                await client.delete_message(msg)
+            await client.send_message(message.channel, 'How many chucks, does it take to chuck a chucking chucker chuck?')
 
 
 client.run(payTheBridgeToll)
